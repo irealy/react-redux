fadbec533c64f33989ab95639370cf33cf71b93b
Bogdan Kononov <b.b.kononov@gmail.com>
2020-08-07 20:54:54 +0300

    update library

---
+++ 01.Todo/README.md
@@ -0,0 +1,1 @@
+Library for TODO application
---
+++ 01.Todo/src/components/todo-item/todo-item.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './todo-item.sass'
+
+const TodoItem = ({ label, important = false}) => {
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
+  };
+
+  return (
+    <span className="todo-item">
+      <span
+        className="todi-item-label"
+        style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
+};
+
+export default TodoItem;

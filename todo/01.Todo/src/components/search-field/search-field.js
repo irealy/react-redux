9c458b5e8324b59c24be66fb3162dde654acf5d5
Bogdan Kononov <b.b.kononov@gmail.com>
2020-08-07 20:56:43 +0300

    update library

--- todo/01.Todo/src/components/search-field/search-field.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './search-field.sass'
-
-const SearchField = () => <input className="form-control search-field" placeholder="search" />;
-
-export default SearchField;

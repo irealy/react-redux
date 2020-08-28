150dc07daf205db086794917913d5f8873aa77f7
Bogdan Kononov <b.b.kononov@gmail.com>
2020-08-28 18:47:54 +0300

    create api data

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
+import SwapiService from 'services/swapi-service';
 
 import './random-planet.sass';
+import Spinner from 'components/spinner/spinner';
 
 export default class RandomPlanet extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+    loading: true,
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet, loading: false });
+  };
+
+  updatePlanet() {
+    const id = 12;
+
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+  }
+
   render() {
+    const {
+      planet: { id, population, rotationPeriod, diameter, name },
+      loading,
+    } = this.state;
+
+    if (loading) {
+      return <Spinner />;
+    }
+
     return (
       <div className='random-planet jumbotron rounded'>
-        <img
-          className='planet-image'
-          src='https://starwars-visualguide.com/assets/img/planets/5.jpg'
-          alt=""
-        />
-        <div>
-          <h4>Planet Name</h4>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>
-              <span className='term'>Population</span>
-              <span>123124</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Rotation Period</span>
-              <span>43</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Diameter</span>
-              <span>100</span>
-            </li>
-          </ul>
-        </div>
+        {loading && <Spinner />}
+        {!loading && (
+          <>
+            <img
+              className='planet-image'
+              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+              alt=''
+            />
+            <div>
+              <h4>{name}</h4>
+              <ul className='list-group list-group-flush'>
+                <li className='list-group-item'>
+                  <span className='term'>Population</span>
+                  <span>{population}</span>
+                </li>
+                <li className='list-group-item'>
+                  <span className='term'>Rotation Period</span>
+                  <span>{rotationPeriod}</span>
+                </li>
+                <li className='list-group-item'>
+                  <span className='term'>Diameter</span>
+                  <span>{diameter}</span>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
       </div>
     );
   }

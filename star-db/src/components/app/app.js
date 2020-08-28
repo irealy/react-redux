ec6df1805cf682a86c12d573df0d1554082edf09
Bogdan Kononov <b.b.kononov@gmail.com>
2020-08-28 16:44:08 +0300

    create structure

---
+++ star-db/src/components/app/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './app.sass';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
